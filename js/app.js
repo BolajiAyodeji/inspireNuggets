5ac6ca7423989bbd2fcab7a5937725eaa09992c9
bolajiayodeji <sirbeejay1@gmail.com>
2018-12-10 15:29:35 +0100

    added json file

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 console.log(data);
-var random = Math.floor(Math.random() * 30);
+var random = Math.floor(Math.random() * 32);
 document.getElementById('quote').innerHTML = data[random].quote;
 document.getElementById('author').innerHTML = data[random].author;
