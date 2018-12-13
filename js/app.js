4abb4a0ed3171c7e7f585e88dd3f7414fdd04284
bolajiayodeji <sirbeejay1@gmail.com>
2018-12-13 10:33:08 +0100

    modified refresh time

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
 console.log(data);
-var random = Math.floor(Math.random() * 32);
+var random = Math.floor(Math.random() * 40);
 document.getElementById('quote').innerHTML = data[random].quote;
 document.getElementById('author').innerHTML = data[random].author;
