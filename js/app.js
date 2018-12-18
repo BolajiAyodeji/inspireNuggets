84886e6ccc2972c86cf071691cab15f74296b6f3
Siphon880gh <wff1012@hotmail.com>
2018-12-18 05:30:53 -0800

    100% json and ajax to clear problems log in Visual Code

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
-var random = Math.floor(Math.random() * data.length);
-document.getElementById('quote').innerHTML = data[random].quote;
-document.getElementById('author').innerHTML = data[random].author;
-console.log(data[random].quote);
-console.log(data[random].author);
+let request = new XMLHttpRequest();
+
+request.open("GET", "assets/quotes.json", true);
+request.onload = () => {
+    if(request.status>=200 && request.status<400) {
+        let data = JSON.parse(request.responseText),
+            random = Math.floor(Math.random() * data.length);
+
+        document.getElementById('quote').innerHTML = data[random].quote;
+        document.getElementById('author').innerHTML = data[random].author;
+        console.log(data[random].quote);
+        console.log(data[random].author);
+    }
+};
+request.onerror = () => {
+    // TODO: Add your on error handler here.
+}
+
+request.send();
